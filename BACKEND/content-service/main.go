48ec5a6c24c7908ca1c69054032bcac1a30b6810
mark-ristic <risticmark1337@gmail.com>
2021-06-07 18:56:42 +0200

    added graceful shutdown

--- BACKEND/content-service/main.go
+++ BACKEND/content-service/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"runtime"
 	"time"
 	"xml/content-service/handlers"
@@ -41,6 +43,18 @@ func main() {
 			os.Exit(1)
 		}
 	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 
 	runtime.Goexit()
 }

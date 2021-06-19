b742fd4bb3bc795e1447284e6681dc551c72496a
dparipovic <danilo.paripovic@yahoo.com>
2021-06-19 18:20:44 +0200

    Finished backend for 2.9 functionality

--- BACKEND/request-service/dto/dtoStructs.go
+++ BACKEND/request-service/dto/dtoStructs.go
@@ -1,9 +1,18 @@
 package dtoRequest
 
+import (
+	"encoding/json"
+	"io"
+)
 
 type FollowRequestDto struct {
 	SentBy string `json:"sentBy"`
 	ForWho     string `json:"forWho"`
+}
+
+type SensitiveContentReportDto struct {
+	PostID string `json:"post_id"`
+	Note string `json:"note"`
 }
 
 type UsernameRoleDto struct {
@@ -13,4 +22,14 @@ type UsernameRoleDto struct {
 
 type ProfileForFollow struct {
 	FollowToUsername string `json:"follow-to-username"`
+}
+
+func (ur *UsernameRoleDto) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(ur)
+}
+
+func (scr *SensitiveContentReportDto) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(scr)
 }

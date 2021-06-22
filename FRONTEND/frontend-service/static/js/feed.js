39df3dfa2be8707df3000bea8434944fb50b27ac
nikola-tomik <nikola.tomik12@gmail.com>
2021-06-22 15:55:55 +0200

    Added showing albums with images and videos

--- FRONTEND/frontend-service/static/js/feed.js
+++ FRONTEND/frontend-service/static/js/feed.js
@@ -55,7 +55,8 @@ function loadFeedContent(){
             xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('myToken'));
         },
         success : function(data) {
-           generatePostsHTML(data)
+           //generatePostsHTML(data)
+           generatePostsHTML1(data)
         },
         error : function() {
             console.log("Erorr at ajax call!")
@@ -125,6 +126,134 @@ function generatePostsHTML(allPosts){
             postsHTML +='<td class="first_td"> <a class="commentedby" href="">' +  comms[j].postedby + '</a>'
             postsHTML += '</td>'
             postsHTML += '<td>' + comms[j].text +'</td>'
+            postsHTML += '</tr>'
+        }
+
+        postsHTML += '</tbody>'
+        postsHTML += '</div>'
+        postsHTML += '</table><hr>'
+        postsHTML += '<div class="writecomment_section"> <div class="form-group">'
+                                                                    //ovaj id ce se verovatno menjati
+        postsHTML += '<textarea class="form-control comment_textarea" id="insert_comment" rows="2"'
+        postsHTML += 'placeholder="Add a comment..."></textarea>'
+        postsHTML += '<button class="btn btn-primary postcommentbtn" id="post_comment_btn"> Post </button>'
+        postsHTML += '</div>'
+        postsHTML += '</div>'
+        postsHTML += '</div>'
+        postsHTML += '</div>'
+        postsHTML += '</div>'
+
+    }
+
+    $("#insertFeedPosts").after(postsHTML)
+
+    
+
+
+
+}
+
+function generatePostsHTML1(allPosts){
+    console.log(allPosts);
+    var postsHTML = ''
+    var img_url
+    var media_string
+
+    for(var i=0; i<allPosts.length;i++){
+
+
+        console.log(img_url)
+        //var img_url1 = '../../../BACKEND/content-service/temp/id-3/3d-render-banner-with-network-communications-low-poly-design.png'
+        //console.log(img_url)
+        // BACKEND/content-service/temp/id-3/3d-render-banner-with-network-communications-low-poly-design.png
+        postsHTML += '<div class="one-feed text-center">' + '\n';
+        postsHTML += '<div class="form-group">' + '\n';
+        postsHTML += '<img src="img/avatar.png" class="profile_img">' + '\n';
+                                            //dodati posle href
+        postsHTML += '<a class="postedby" href="">' + allPosts[i].postedby + '</a>\n';
+        postsHTML += '</div>' + '\n';
+
+        postsHTML += '<div class="card">' + '\n';
+            //          ovde treba ici allPosts[i].medias[j].path,  ubaciti jos jednu for petlju, ali trenutno putanje nisu sredjene
+            //CAROUSEL deo
+        //postsHTML += media_string
+
+        postsHTML +=  `<div id="demo-${i}" class="carousel slide" data-ride="carousel">`
+        postsHTML +=  `<ul class="carousel-indicators">`
+
+        console.log(allPosts[i].medias)
+        for(var j=0; j < allPosts[i].medias.length ; j++) {
+
+            if(j==0) {
+                postsHTML += `<li data-target="#demo-${i}" data-slide-to="${j}" class="active"></li>`
+            } else {
+                postsHTML += `<li data-target="#demo-${i}" data-slide-to="${j}" ></li>`    
+            }
+
+        }
+
+        postsHTML +=  `</ul>`
+
+        // SLIDESHOW:
+
+        postsHTML += `<div class="carousel-inner" style=" width:100%; height:auto !important;">`
+        
+        for(var g=0; g < allPosts[i].medias.length; g++) {
+            img_url = '../' + allPosts[i].medias[g].path
+            media_string = `<img width="100%" height="auto; !important" src="${img_url}" alt="Card image cap">`
+    
+            if (allPosts[i].medias[g].path.split(".")[1] != 'jpg' && allPosts[i].medias[g].path.split(".")[1] != 'png') {
+                // then its a video
+                media_string = `
+                            <video width="70%" height="100%" controls>
+                                <source src="${img_url}" type="video/mp4">                       
+                                Your browser does not support the video tag.
+                            </video>`
+            }
+
+
+            if(g==0) {
+                postsHTML += `<div class="carousel-item active">`
+                postsHTML +=  media_string
+                postsHTML +=  `</div>`
+            } else {
+                postsHTML += `<div class="carousel-item">`
+                postsHTML +=  media_string
+                postsHTML +=  `</div>`
+            }
+
+        }
+
+        postsHTML += `</div>`
+
+        postsHTML += `
+                            <a class="carousel-control-prev" href="#demo-${i}" data-slide="prev">
+                                <span class="carousel-control-prev-icon"  style="background-color: black; border: 1px white;"></span>
+                            </a>
+                            <a class="carousel-control-next" href="#demo-${i}" data-slide="next" >
+                                <span class="carousel-control-next-icon" style="background-color: black; border: 1px white;"></span>
+                            </a>`
+
+        postsHTML += `</div>`
+            // CAROUSEL END --> card-body mi treba isti
+        postsHTML += '<div class="card-body"></div>' + '\n';
+        postsHTML += '<p class="card-text text-left description_part">' + allPosts[i].description + '</p>' + '\n';
+        postsHTML += '<p class="text-left"></p>'
+        postsHTML += '<a href=""> Like &nbsp&nbsp&nbsp&nbsp</a></p><hr>' + '\n';
+        postsHTML += '<h5 class="text-left">Comments:</h5><hr>' + '\n';
+        postsHTML += '<table class="table" id="mydatatable">'
+        postsHTML += '<thead class="hideheader">'
+        postsHTML += '<tr><th scope="col" id="commented_by">CommentedBy</th><th scope="col" id="comment">Comment</th></tr></thead>'
+        postsHTML += '<div class="bodycontainer scrollable">'
+        postsHTML += '<tbody class="text-left">'
+
+        for(var h=0; h<allPosts[i].comments.length; h++){
+            var comms = allPosts[i].comments
+            postsHTML += '<tr>'
+                                                //dodati posle href za profil
+            postsHTML +='<td class="first_td"> <a class="commentedby" href="">' +  comms[h].postedby + '</a>'
+            postsHTML += '</td>'
+            postsHTML += '<td>' + comms[h].text +'</td>'
             postsHTML += '</tr>'
         }
 

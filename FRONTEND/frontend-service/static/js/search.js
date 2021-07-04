26fdd1894a64b59f95b263c5ed805d0a53008ecb
mark-ristic <risticmark1337@gmail.com>
2021-07-04 15:54:49 +0200

    prepared front for search

---
+++ FRONTEND/frontend-service/static/js/search.js
@@ -0,0 +1,63 @@
+const search_field = $('#search_input')
+var public_profiles 
+var public_names = {}
+var allowed_names = {}
+var allowed_profiles
+var final_profiles = {}
+
+search_field.focusin(function() {
+
+	console.log('focus')
+})
+
+
+function who_am_I() {
+
+	$.ajax({
+		type: 'GET',
+		crossDomain: true,
+		url: AUTH_SERVICE_URL + '/whoami',
+		contentType: 'application/json',
+		dataType: 'JSON',
+		beforeSend: function (xhr) {
+		    xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('myToken'));
+		},
+		success: function (data) {
+		    console.log('using authenticated search..')
+		    authenticatedSearch(data)
+		},
+		error: function () {
+		    console.log('using non authenticated search..')
+		    nonAuthenticatedSearch(data)
+		}
+	    })
+
+}
+
+function authenticatedSearch() {
+	getAllPublicProfiles()
+	getAllAllowedProfiles()
+	getAllPublicPosts()
+	getAllAllowedPosts()
+}
+
+function nonAuthenticatedSearch() {
+	getAllPublicProfiles()
+	getAllPublicPosts()
+}
+
+function getAllPublicProfiles() {
+
+}
+
+function getAllPublicPosts() {
+
+}
+
+function getAllAllowedProfiles() {
+
+}
+
+function getAllAllowedPosts() {
+
+}

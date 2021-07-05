b737e38d0f103c449982f6849dae3b3c5b5ce281
mark-ristic <risticmark1337@gmail.com>
2021-07-05 16:10:50 +0200

    Added filtering for profiles

--- FRONTEND/frontend-service/static/js/search.js
+++ FRONTEND/frontend-service/static/js/search.js
@@ -1,11 +1,15 @@
 const search_field = $('#search_input')
-var public_profiles 
-var allowed_profiles
+var public_profiles = []
+var allowed_profiles = []
 var public_names = {}
 var allowed_names = {}
-var final_profiles = {}
+var final_profiles = []
 var final_posts = []
 var current_input = 'valjda_nece_ovo_nikad_upisati13151q6161q67'
+const profile_list = $('#profile_list')
+var location_counter = 0
+var tag_counter = 0
+
 
 
 $(document).ready(function() {
@@ -39,8 +43,23 @@ search_field.keyup(function(){
 
 })
 
-function filterByEverything() {
+function filterUsers() {
 
+	const searchInput = search_field.val().toLowerCase().trim();
+	const searchString = searchInput.replaceAll('@', '').trim()
+	//console.log(searchString)
+	const filtered_users = final_profiles.filter((profile) => {
+		return (
+			profile.username.toLowerCase().includes(searchString)
+			);
+		});
+	fillColumnsProfiles(filtered_users);
+
+}
+
+function filterByEverything() {
+	emptyByTags()
+	emptyByLocation()
 	const searchString = search_field.val().toLowerCase().trim();
 	//console.log(searchString)
 	const filtered_posts = final_posts.filter((post) => {
@@ -51,6 +70,8 @@ function filterByEverything() {
 			post.postedby.toLowerCase().includes(searchString)
 			);
 		});
+	
+	filterUsers()
 	fillColumnsTags(filtered_posts, false);
 	fillColumnsLocation(filtered_posts, false)
 
@@ -59,7 +80,7 @@ function filterByEverything() {
 }
 
 function filterByLocation() {
-
+	emptyByLocation()
 	const searchInput = search_field.val().toLowerCase().trim();
 	const searchString = searchInput.replaceAll('^', '').trim()
 	//console.log(searchString)
@@ -74,7 +95,7 @@ function filterByLocation() {
 }
 
 function filterByTags() {
-
+	emptyByTags()
 	const searchString = search_field.val().toLowerCase().trim();
 	//console.log(searchString)
 	const filtered_posts = final_posts.filter((post) => {
@@ -141,6 +162,14 @@ function getAllPublicProfiles() {
 		},
 		success: function (data) {
 		    //console.log(data)
+			data.forEach(function(d) {
+				if (final_profiles.filter(e => e.username == d.username).length == 0) {
+					final_profiles.push(d)
+				}		  
+			})
+			console.log('Size of final_profiles is ' + final_posts.length)
+			//public_profiles.push.apply(public_profiles, data)
+			fillColumnsProfiles(final_profiles)
 		    getAllPublicPosts(data)
 		},
 		error: function () {
@@ -186,22 +215,35 @@ function getAllAllowedPosts() {
 
 }
 
+function emptyByLocation() {
+	$('#column-right-0').html('')
+	$('#column-right-1').html('')
+	$('#column-right-2').html('')
+	$('#column-right-3').html('')
+	location_counter = 0
+}
+
+function emptyByTags() {
+	$('#column-left-0').html('')
+	$('#column-left-1').html('')
+	$('#column-left-2').html('')
+	$('#column-left-3').html('')
+	tag_counter = 0
+}
+
 function fillColumnsLocation(pics, sentByAjax) {
 	console.log('%c showing by location', 'color:blue')
 	console.log(pics)
 	if (sentByAjax)
 		final_posts.push.apply(final_posts, pics)
 
-	$('#column-right-0').html('')
-	$('#column-right-1').html('')
-	$('#column-right-2').html('')
-	$('#column-right-3').html('')
 	var column 
 	var html = ''
 	var coldiv
 	for(let i = 0; i<pics.length; i++) {
 		
-		column = "#column-right-" + i % 4
+		column = "#column-right-" + location_counter % 4
+		location_counter = location_counter + 1
 		coldiv = $(column)
 		coldiv.append(`<img src="${pics[i].medias[0].path}">`)
 		//coldiv.append(testList[i])
@@ -214,20 +256,30 @@ function fillColumnsTags(pics, sentByAjax) {
 	console.log(pics)
 	if (sentByAjax)
 		final_posts.push.apply(final_posts, pics)
-
-	$('#column-left-0').html('')
-	$('#column-left-1').html('')
-	$('#column-left-2').html('')
-	$('#column-left-3').html('')
+	
 	var column 
 	var html = ''
 	var coldiv
 	for(let i = 0; i<pics.length; i++) {
 		
-		column = "#column-left-" + i % 4
+		column = "#column-left-" + tag_counter % 4
+		tag_counter = tag_counter + 1
 		coldiv = $(column)
 		coldiv.append(`<img src="${pics[i].medias[0].path}">`)
 		//coldiv.append(testList[i])
 	}
 
+}
+
+function fillColumnsProfiles(profiles) {
+	profile_list.html('')
+	// <li class="list-group-item">Cras justo odio</li>
+	profiles.forEach(function(p){
+
+		profile_list.append(`<li class="list-group-item" id="user-${p.username}" onclick="redirectToUser(this.id)">${p.username}</li>`)
+	})
+}
+
+function redirectToUser(id) {
+	console.log(id)
 }
